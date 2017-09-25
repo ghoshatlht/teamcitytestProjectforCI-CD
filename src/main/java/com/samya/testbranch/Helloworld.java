0ddbfdf610ddfd8174f66ecf671637388ef1395b
SG05477 <SG05477@denb0271.de.corp.danet.com>
2017-09-25 15:40:23 +0200

    branches

---
+++ src/main/java/com/samya/testbranch/Helloworld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.samya.testbranch;
+
+/**
+ * @author SG05477
+ *
+ */
+public class Helloworld {
+	
+	public static void main(String args[])
+	{
+		
+		System.out.println("This will Test branches");
+		
+	}
+
+}
